db826c4a0658d7a4decd8b0bcd08bfe17f0081dc
ALmin <bigal493905@gmail.com>
2021-07-02 00:27:15 +0800

    修改+註解

--- index.js
+++ index.js
@@ -1,26 +1,27 @@
-const { Router } = require('express')
 let express = require('express')
 let mysqlAPI = require('./routes/mysqlAPI')
 
 let app = express()
 
+//使用art-template模板引擎
 app.engine('html', require('express-art-template'))
 
+//開始監聽3000 port
 app.listen(3000, () => {
     console.log('App is listening at localhost:3000.')
 })
 
+//將public資料夾內的檔案變成靜態檔案
 app.use(express.static(__dirname + '/public'))
 
-app.use(express.json())
-app.use(express.urlencoded({extended: false}))
-
+//使用router
 app.use('/todolist', mysqlAPI)
 
+//首頁
 app.get('/', (req, res) => {
     res.render('index.html')
 })
 
-app.get('*', (req, res, next) => {
+app.get('*', (req, res) => {
     res.render('nopage.html')
 })
--- routes/mysqlAPI.js
+++ routes/mysqlAPI.js
@@ -2,21 +2,28 @@ let express = require('express')
 let mysql = require('mysql2')
 let mysqlAPI = express.Router()
 
+//由於req中不會含有json檔，所以註解掉了
+//mysqlAPI.use(express.json())
+
+//使用中間件將HTML form表單提交的數據取出
+mysqlAPI.use(express.urlencoded({extended: false}))
+
+//設定連線mysql資料庫的資訊
 let connection = mysql.createConnection({
     host    :'localhost',
     user    :'root',
     password:'root'
 })
 
-let path
-
+//連上mysql資料庫
 connection.connect((err) =>{
     if(err) throw err
     else console.log('Connecting Mysql successfully.')
 })
 
+//To-Do List的首頁，並且從資料庫查詢list，然後放入HTML
 mysqlAPI.get('/', (req, res) => {
-    connection.query('select id, msg from todolist.todo', (err, rows) => {
+    connection.query(`select id, msg from todolist.todo`, (err, rows) => {
         if(err) throw err
         else res.render('todolist.html', {
             data:rows
@@ -24,6 +31,7 @@ mysqlAPI.get('/', (req, res) => {
     })
 })
 
+//新增內容進資料庫
 mysqlAPI.post('/post', (req, res) => {
     connection.query('insert into todolist.todo (msg) values (?)', req.body.text, (err) => {
         if(err) throw err
@@ -31,6 +39,7 @@ mysqlAPI.post('/post', (req, res) => {
     res.redirect('.')
 })
 
+//刪除內容，以id當作要刪除的依據
 mysqlAPI.post('/delete/:id', (req, res) => {
     connection.query('delete from todolist.todo where id = ?', req.params.id, (err) => {
         if(err) throw err
@@ -38,6 +47,7 @@ mysqlAPI.post('/delete/:id', (req, res) => {
     res.redirect('..')
 })
 
+//修改內容
 mysqlAPI.post('/modify/:id', (req, res) => {
     connection.query('update todolist.todo set msg = ? where id = ?', [req.body.value, req.params.id], (err) => {
         if(err) throw err
